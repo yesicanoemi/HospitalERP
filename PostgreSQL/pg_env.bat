5f2e73887d83f96ee6acaae521b87361ec018a58
Yesinoemi <yesicargueta@gmail.com>
2016-04-27 11:14:10 -0600

    Base de datos hospital
    Signed-off-by: Yesinoemi <yesicargueta@gmail.com>

---
+++ PostgreSQL/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="C:\Program Files (x86)\OpenERP 7.0-20140221-003045\PostgreSQL\bin";%PATH%
+@SET PGDATA=C:\Program Files (x86)\OpenERP 7.0-20140221-003045\PostgreSQL\data
+@SET PGDATABASE=postgres
+@SET PGUSER=openpg
+@SET PGPORT=5432
+@SET PGLOCALEDIR=C:\Program Files (x86)\OpenERP 7.0-20140221-003045\PostgreSQL\share\locale
+
+                          
